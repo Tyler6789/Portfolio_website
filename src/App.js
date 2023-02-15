88be7eb1d5d881173c9559b481d29338546a9d38
Tyler <wiggintonstudios@gmail.com>
2023-02-15 10:03:47 -0700

    Cleaning up files, addition of ionic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import './App.css';
-function App() {
+import { IonApp, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonTitle, IonToolbar } from '@ionic/react';
+import {useState } from 'react';
 
+
+function App() {
+  const [name, setName] = useState();
+  return (
+    <IonApp>
+      <IonHeader>
+        
+      </IonHeader>
+      <IonContent className="ion-padding">
+      </IonContent>
+    </IonApp>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-
-const Header = () => {
-  return (
-    <>
-      <h1>Hello There!</h1>
-      <p>This is my page.</p>
-    </>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
